cd307285244f864b3d21a3a4cf5e451c38c9da70
B. <B.@192.168.8.128>
2025-12-10 00:30:08 +0300

    First commit R Person class

---
+++ src/srms/Person.java
@@ -0,0 +1,72 @@
+
+
+public class Person{
+    
+    private String name;
+    private int id ;
+    private int age ;
+    
+    public Person(String name , int id , int age){
+        this.name = name ;
+        this.id = id ;
+        this.age = age ;
+    }
+    public String getName(){
+        return name;
+    }
+    
+    public int getId(){
+        return id;
+        
+    }
+    
+    public int getAge(){
+        return age ;
+    }
+    
+              public void setName(String name){
+        try{
+            if(name == null || name.isEmpty()){
+                throw new Exception("name cannot be emptu!!");
+            }
+            this.name = name;
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        }
+    
+    public void setId(int id){
+        
+        try{
+            if (id <= 0 ) throw new Exception("ID be positive");
+            this.id = id;
+        }
+        catch (Exception a){
+            System.out.println(a.getMessage());
+        }
+    }
+    
+    public void setAge(int age){
+        try {
+            if (age < 0) throw new Exception("age cnat be negative");
+            this.age = age ;
+        }
+        catch (Exception c){
+            System.out.println(c.getMessage());
+        }
+    }
+    
+    
+  
+    
+    public void display(){
+System.out.println("student info");
+        System.out.println("NAME" + name );
+                System.out.println("ID " + id);
+System.out.println(" AGE " + age);
+
+}
+   
+}
+
