d1996aedce52461f5b5ee8a1b0fe7eec81bb59a3
almas <almas@LAPTOP-0661BP6P>
2025-12-12 20:23:41 +0300

    add student class

--- src/srms/Student.java
+++ src/srms/Student.java
@@ -1,55 +1,44 @@
-
 package srms;
-public class Student  extends Person {
-    
-    private String major ;
-    private double gpa ;
-    
-    public Student(String name , int id , int age , String major , double gpa){
-        super(name , id , age );
-        this.major = major ;
+
+public class Student extends Person {
+
+    private String major;
+    private double gpa;
+
+    public Student(String name, int id, int age, String major, double gpa) {
+        super(name, id, age);
+        this.major = major;
         this.gpa = gpa;
     }
-    
-    public String getMajor(){
-        return major;
-    }
-    
-    public double getGpa(){
-        return gpa;
-    }
-    
-    public void setMajor(String major){
+
+    public String getMajor() { return major; }
+    public double getGpa() { return gpa; }
+
+    public void setMajor(String major) {
         try {
-            if(major == null  || major.isEmpty()){
-                throw new Exception("major cnnot be empty");
-            }
-                            this.major = major;
-}
-            catch (Exception m){
-                    System.out.println(m.getMessage());
-                    }
-        
+            if (major == null || major.isEmpty())
+                throw new Exception("Major cannot be empty");
+            this.major = major;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
-    
-    public void setGpa(double gpa){
-        try{
-            if (gpa < 0.0 || gpa > 5.0 ){
-                throw new Exception ("must be between 0 and 5 ");
-            }
+
+    public void setGpa(double gpa) {
+        try {
+            if (gpa < 0 || gpa > 5)
+                throw new Exception("GPA must be between 0 and 5");
             this.gpa = gpa;
-        }
-        catch (Exception g){
-            System.out.println(g.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
-    
+
     @Override
-    public void display(){
-        System.out.println("student infor");
-                System.out.println("MAJPR" + major);
-                        System.out.println("GPA" + gpa);
-
-
+    public void display() {
+        super.display();
+        System.out.println("Major: " + major);
+        System.out.println("GPA  : " + gpa);
+        System.out.println("------------------");
     }
 }
