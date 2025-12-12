c3187ab0739e6d333714757ff48517093ca4bbb0
almas <almas@LAPTOP-0661BP6P>
2025-12-12 20:23:06 +0300

    add srms class

---
+++ src/srms/SRMS.java
@@ -0,0 +1,58 @@
+package srms;
+
+import java.util.ArrayList;
+
+public class SRMS {
+
+    private final ArrayList<Student> students;
+    private final FileHandler fileHandler;
+    private final AutoSaver autoSaver;
+
+    public SRMS() {
+        fileHandler = new FileHandler("students.ser");
+        students = fileHandler.load();
+        autoSaver = new AutoSaver(this);
+        autoSaver.start();
+    }
+
+    public void addStudent(Student s) {
+        students.add(s);
+    }
+
+    public void removeStudent(int id) {
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getId() == id) {
+                students.remove(i);
+                return;
+            }
+        }
+    }
+
+    public void updateStudent(int id, String name, int age, String major, double gpa) {
+        for (Student s : students) {
+            if (s.getId() == id) {
+                s.setName(name);
+                s.setAge(age);
+                s.setMajor(major);
+                s.setGpa(gpa);
+                return;
+            }
+        }
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void save() {
+        fileHandler.save(students);
+    }
+
+    void autoSave() {
+        fileHandler.save(students);
+    }
+
+    public void stopAutoSave() {
+        autoSaver.stopSaving();
+    }
+}
